d84a081fa2c70fc8035cc8070b8ecc207191fc05
Zakhar Portianov <kkragy@gmail.com>
2017-09-11 16:40:24 +0300

    Update data.js
    Add quotes

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
-{data: [
-  {id:123, name:"Vasya"},
-  {id:13, name:"Oleg"},
-  {id:12, name:"Tanya"},
-  {id:11, name:"Alex"},
-  {id:10, name:"Volodymir"},
-  {id:9, name:"Inna"},
-  {id:8, name:"Fedor"},
-  {id:7, name:"Taras"},
+{"data": [
+  {"id":123, "name":"Vasya"},
+  {"id":13, "name":"Oleg"},
+  {"id":12, "name":"Tanya"},
+  {"id":11, "name":"Alex"},
+  {"id":10, "name":"Volodymir"},
+  {"id":9, "name":"Inna"},
+  {"id":8, "name":"Fedor"},
+  {"id":7, "name":"Taras"},
 ]}
